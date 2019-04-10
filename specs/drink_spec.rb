987a56140846ff8cbfb7b8a0b809a412cb8aa76e
James Swatton <jamessswatton@gmail.com>
2019-04-10 11:39:09 +0100

    initial commit - add specs and class files

---
+++ specs/drink_spec.rb
@@ -0,0 +1,12 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../drink')
+
+class TestDrink < MiniTest::Test
+
+  def test_get_drink_name
+    drink1 = Drink.new("guiness", 3)
+    assert_equal("guiness", drink1.name)
+  end
+
+end
